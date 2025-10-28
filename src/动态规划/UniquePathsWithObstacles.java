a6193f82af1e5d2045422a7d7e2c555f7c0fc233
JH.Zhai <junhuai.zhai@beyond-power.com>
2025-10-28 20:22:56 +0800

    dp:UniquePathsWithObstacles(dp double arr)

--- src/动态规划/UniquePathsWithObstacles.java
+++ src/动态规划/UniquePathsWithObstacles.java
@@ -33,24 +33,84 @@ public class UniquePathsWithObstacles {
         System.out.println(iterator(obstacleGrid));
     }
 
-//    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-//
-//    }
+
+    public static int iterator_opt_memo(int[][] obstacleGrid){
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
+            return 0;
+        }
+
+        // 拆分子问题：从i到达j位置的路径数
+        int[][] dp = new int[m][n];
+        // dp[i][j] = dp[i - 1][j] + dp[i][j - 1]  所以要初始化左边跟上面的第一行数据
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                dp[0][j] = 0;
+            } else {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+        // 填充其他数据
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j]  == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
 
     public static int iterator(int[][] obstacleGrid){
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
+            return 0;
+        }
+
+        // 拆分子问题：从i到达j位置的路径数
         int[][] dp = new int[m][n];
-        dp[m - 1][n - 1] = 1;
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                if (obstacleGrid[i][j] == 1) {
-                    continue;
-                }
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+        // dp[i][j] = dp[i - 1][j] + dp[i][j - 1]  所以要初始化左边跟上面的第一行数据
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = dp[i - 1][0];
             }
         }
-        return dp[0][0];
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                dp[0][j] = 0;
+            } else {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+        // 填充其他数据
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j]  == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 
     public static int traverse(int[][] obstacleGrid, int i, int j, int[][] memo) {
