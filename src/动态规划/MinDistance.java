939fdc5c5070b834ae27487f6d753b6274642b51
JH.Zhai <junhuai.zhai@beyond-power.com>
2025-12-19 13:51:39 +0800

    dp: minDistance

---
+++ src/动态规划/MinDistance.java
@@ -0,0 +1,79 @@
+package 动态规划;
+
+/**
+ * 72. 编辑距离 中等
+ *
+ * 给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+ *
+ * 你可以对一个单词进行如下三种操作：
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ *
+ * 示例 1：
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ *
+ * 示例 2：
+ * 输入：word1 = "intention", word2 = "execution"
+ * 输出：5
+ * 解释：
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ *
+ *
+ * 提示：
+ * 0 <= word1.length, word2.length <= 500
+ * word1 和 word2 由小写英文字母组成
+ */
+public class MinDistance {
+    public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
+        System.out.println(new MinDistance().minDistance(word1, word2));
+    }
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            char a = word1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                char b = word2.charAt(j - 1);
+                if (a == b) {
+                    dp[i][j] = Math.min(dp[i][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i - 1][j - 1]));
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i - 1][j - 1] + 1));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
