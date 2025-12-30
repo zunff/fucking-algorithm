dd779e5291b3e5f6f47e2546b9b7e140609ae57d
JH.Zhai <junhuai.zhai@beyond-power.com>
2025-12-30 11:15:39 +0800

    dp: 416 CanPartition

---
+++ src/动态规划/A二维序列DP/CanPartition416.java
@@ -0,0 +1,68 @@
+package 动态规划.A二维序列DP;
+
+/**
+ * 416. 分割等和子集  中等
+ *
+ * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ *
+ * 示例 1：
+ * 输入：nums = [1,5,11,5]
+ * 输出：true
+ * 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+ *
+ * 示例 2：
+ * 输入：nums = [1,2,3,5]
+ * 输出：false
+ * 解释：数组不能分割成两个元素和相等的子集。
+ *
+ *
+ * 提示：
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 100
+ */
+public class CanPartition416 {
+    public static void main(String[] args) {
+        int[] nums = {1,5,11,5};
+        System.out.println(new CanPartition416().canPartition(nums));
+    }
+
+    public boolean canPartitionSecond(int[] nums) {
+        return false;
+    }
+
+    /**
+     * 转化为 0 1 背包，容量为 sum / 2
+     * dp[i][j]  表示前i个数字，是否能组成 j
+     * 边界：dp[0][0] = true
+     *      i = 0 时  没有数字不能组成 j，false
+     *      j = 0 时  数字可以不选 能组成0，true
+     * {
+     *     j >= v：dp[i][j] = dp[i - 1][j - v] || dp[i - 1][j]
+     *     j < v： dp[i][j] = dp[i - 1][j]
+     * }
+     */
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int v = sum / 2;
+        int n = nums.length;
+        boolean[] dp = new boolean[v + 1];
+        dp[0] = true;
+
+        // 注意 01背包 是内层倒序
+        for (int i = 1; i <= n; i++) {
+            for (int j = v; j >= 1; j--) {
+                int num = nums[i - 1];
+                if (j >= num) {
+                    dp[j] = dp[j - num] || dp[j];
+                }
+            }
+        }
+        return dp[v];
+    }
+}
