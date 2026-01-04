ab72c0016d51c24063397de4cbfe906e03704a62
JH.Zhai <junhuai.zhai@beyond-power.com>
2026-01-04 11:57:00 +0800

    dp(Third): 516 LongestPalindromeSubseq

--- src/动态规划/A二维序列DP/LongestPalindromeSubseq516.java
+++ src/动态规划/A二维序列DP/LongestPalindromeSubseq516.java
@@ -25,14 +25,31 @@ public class LongestPalindromeSubseq516 {
 
     public static void main(String[] args) {
         String s = "bbbab";
-        System.out.println(new LongestPalindromeSubseq516().longestPalindromeSubseqSecond(s));
+        System.out.println(new LongestPalindromeSubseq516().longestPalindromeSubseqThird(s));
     }
 
 
-
-
+    /**
+     * dp[i][j] 表示 s[i...j]的最长回文字串的长度
+     * {
+     *     s[i] == s[j]     dp[i][j] = dp[i + 1][j - 1] + 2
+     *     s[i] != s[j]     dp[i][j] = max(dp[i][j - 1], dp[i + 1][j])
+     * }
+     */
     public int longestPalindromeSubseqThird(String s) {
-        return 0;
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = i + 1 < n && j - 1 >= 0 ? dp[i + 1][j - 1] + 2 : 0;
+                } else {
+                    dp[i][j] = Math.max(j - 1 >= 0 ? dp[i][j - 1] : 0, i + 1 < n ? dp[i + 1][j] : 0);
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
 
 
