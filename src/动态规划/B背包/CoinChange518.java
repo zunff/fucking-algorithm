f32f145918202f3136afca675c993ca05e8d4d68
JH.Zhai <junhuai.zhai@beyond-power.com>
2026-01-07 10:09:21 +0800

    dp: 518 CoinChangeⅡ

---
+++ src/动态规划/B背包/CoinChange518.java
@@ -0,0 +1,149 @@
+package 动态规划.B背包;
+
+/**
+ * 518. 零钱兑换 II 中等
+ *
+ * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+ * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+ * 假设每一种面额的硬币有无限个。
+ * 题目数据保证结果符合 32 位带符号整数。
+ *
+ * 示例 1：
+ * 输入：amount = 5, coins = [1, 2, 5]
+ * 输出：4
+ * 解释：有四种方式可以凑成总金额：
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ *
+ * 示例 2：
+ * 输入：amount = 3, coins = [2]
+ * 输出：0
+ * 解释：只用面额 2 的硬币不能凑成总金额 3 。
+ *
+ * 示例 3：
+ * 输入：amount = 10, coins = [10]
+ * 输出：1
+ *
+ * 提示：
+ * 1 <= coins.length <= 300
+ * 1 <= coins[i] <= 5000
+ * coins 中的所有值 互不相同
+ * 0 <= amount <= 5000
+ */
+public class CoinChange518 {
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        System.out.println(new CoinChange518().change(5, coins));
+    }
+
+
+    public int changeSecond(int amount, int[] coins) {
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * dp[i][j] 表示前 i 个硬币，在容量 j 下有几种组合方式
+     * 初始化：dp[0][0] = 1     表示一个都不选
+     *        dp[i][0] = 1     表示一个都不选
+     *        dp[0][j] = 0     amount == 0 的情况已经在dp[0][0] 考虑了
+     * {
+     *    j >= coins[i]     dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]]
+     *    j < coins[i]      dp[i][j] = dp[i - 1][j]
+     * }
+     */
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int coin = coins[i - 1];
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+
+            }
+        }
+        return dp[amount];
+
+
+
+
+//        int n = coins.length;
+//        int[][] dp = new int[n + 1][amount + 1];
+//        for (int i = 0; i <= n; i++) {
+//            dp[i][0] = 1;
+//        }
+//
+//        for (int i = 1; i <= n; i++) {
+//            int coin = coins[i - 1];
+//            for (int j = 1; j <= amount; j++) {
+//                dp[i][j] = dp[i - 1][j];
+//                if (j >= coin) {
+//                    dp[i][j] += dp[i][j - coin];
+//                }
+//            }
+//        }
+//        return dp[n][amount];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
