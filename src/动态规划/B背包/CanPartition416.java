ac78874889a0eec641295b6bb9c654710d73a682
JH.Zhai <junhuai.zhai@beyond-power.com>
2026-01-08 11:29:56 +0800

    dp(Third): 416 CanPartition

--- src/动态规划/B背包/CanPartition416.java
+++ src/动态规划/B背包/CanPartition416.java
@@ -1,5 +1,7 @@
 package 动态规划.B背包;
 
+import java.util.Arrays;
+
 /**
  * 416. 分割等和子集  中等  2025/12/29
  *
@@ -22,14 +24,65 @@ package 动态规划.B背包;
  */
 public class CanPartition416 {
     public static void main(String[] args) {
-        int[] nums = {1,5,11,5};
-        System.out.println(new CanPartition416().canPartitionSecond(nums));
+        int[] nums = {2,2,1,1};
+        System.out.println(new CanPartition416().canPartitionThird(nums));
     }
 
 
-
+    /**
+     * 定义 nums 总和为 sum
+     *
+     * 将题目转换为，找几个数，使得他们之和为 sum / 2，即 01 背包问题
+     * 无解条件：sum % 2 != 0
+     *
+     * dp[i][j] 为 前 i个数，是否能找出几个数相加结果为 j
+     * 初始化：dp[0][0] = true
+     *      i > 0 dp[i][0] = true   一个都不选
+     *      j > 0 dp[0][j] = false
+     * {
+     *     j >= nums[i]     dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]
+     *     j < nums[i]      dp[i][j] = dp[i - 1][j]
+     * }
+     */
     public boolean canPartitionThird(int[] nums) {
-        return false;
+        int n = nums.length;
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int v = sum / 2;
+        boolean[] dp = new boolean[v + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = v; j >= nums[i - 1]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i - 1]];
+
+            }
+        }
+        return dp[v];
+
+
+//        int n = nums.length;
+//        int sum = Arrays.stream(nums).sum();
+//        if (sum % 2 != 0) {
+//            return false;
+//        }
+//        int v = sum / 2;
+//        boolean[][] dp = new boolean[n + 1][v + 1];
+//        for (int i = 0; i <= n; i++) {
+//            dp[i][0] = true;
+//        }
+//
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= v; j++) {
+//                dp[i][j] = dp[i - 1][j];
+//                if (j >= nums[i - 1]) {
+//                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+//                }
+//            }
+//        }
+//        return dp[n][v];
     }
 
 
