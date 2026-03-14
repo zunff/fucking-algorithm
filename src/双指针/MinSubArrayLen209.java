b14b47867d9b44a43079f3d88d89289247cd2651
zunf <1430119384@qq.com>
2026-03-14 15:11:15 +0800

    tp: 209 MinSubArrayLen

---
+++ src/双指针/MinSubArrayLen209.java
@@ -0,0 +1,135 @@
+package 双指针;
+
+/**
+ * 209. 长度最小的子数组 中等
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * 找出该数组中满足其总和大于等于 target 的长度最小的 子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ *
+ * 示例 1：
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ *
+ * 示例 2：
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ *
+ * 示例 3：
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ *
+ * 提示：
+ * 1 <= target <= 10^9
+ * 1 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^4
+ *
+ * 进阶：
+ * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+ */
+public class MinSubArrayLen209 {
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,2,4,3};
+        System.out.println(new MinSubArrayLen209().minSubArrayLen(7, nums));
+    }
+
+    public int minSubArrayLenSecond(int target, int[] nums) {
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int sum = 0;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0, j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            while (i <= j && sum >= target) {
+                ans = Math.min(ans, j - i + 1);
+                sum -= nums[i];
+                i++;
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
