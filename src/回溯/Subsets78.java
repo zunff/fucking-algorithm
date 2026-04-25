c8468641ec19e35505774896773a99862af4c54f
zunf <1430119384@qq.com>
2026-04-25 11:42:19 +0800

    backTracking: 78 Subsets

---
+++ src/回溯/Subsets78.java
@@ -0,0 +1,65 @@
+package 回溯;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 78. 子集 中等
+ *
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ *
+ * 示例 2：
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ *
+ * 提示：
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * nums 中的所有元素 互不相同
+ */
+public class Subsets78 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(new Subsets78().subsets(nums));
+    }
+
+    private final List<Integer> cur = new ArrayList<>();
+    private final List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        backTracking(nums, 0);
+        return ans;
+    }
+
+    public void backTracking(int[] nums, int begin) {
+        ans.add(new ArrayList<>(cur));
+
+        for (int i = begin; i < nums.length; i++) {
+            cur.add(nums[i]);
+            backTracking(nums, i + 1);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
