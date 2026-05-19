be9828732778da47daee198b7097762e0318bdda
zunf <1430119384@qq.com>
2026-05-19 16:55:08 +0800

    dp: 198 Rob

---
+++ src/动态规划/hot100/Rob198.java
@@ -0,0 +1,67 @@
+package 动态规划.hot100;
+
+/**
+ * 198. 打家劫舍 中等
+ *
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ *
+ * 示例 1：
+ * 输入：[1,2,3,1]
+ * 输出：4
+ * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+ *      偷窃到的最高金额 = 1 + 3 = 4 。
+ *
+ * 示例 2：
+ * 输入：[2,7,9,3,1]
+ * 输出：12
+ * 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+ *      偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ *
+ * 提示：
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
+ */
+public class Rob198 {
+    public static void main(String[] args) {
+        System.out.println(new Rob198().rob(new int[]{1,2,3,1}));
+    }
+
+    /**
+     * rob[i] 表示前 i 个房间中，选择偷第i个房间，能偷到的最高金额
+     * noRob[i] 表示前 i 个房间中，选择不偷第i个房间，能偷到的最高金额
+     * {
+     *     rob[i] = noRob[i - 1] + nums[i]
+     *     noRob[i] = max(rob[i - 1], noRob[i - 1])
+     * }
+     */
+    public int rob(int[] nums) {
+        int rob = nums[0];
+        int noRob = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            int preRob = rob;
+            rob = noRob + nums[i];
+            noRob = Math.max(preRob, noRob);
+        }
+        return Math.max(rob, noRob);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
