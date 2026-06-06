38b6ca90674b7c8598e19ed7e66f592d1cba26d9
zunf <1430119384@qq.com>
2026-06-06 10:25:43 +0800

    greedy: 121 MaxProfit

---
+++ src/贪心/hot100/MaxProfit121.java
@@ -0,0 +1,70 @@
+package 贪心.hot100;
+
+/**
+ * 121. 买卖股票的最佳时机 简单
+ *
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * 示例 1：
+ * 输入：[7,1,5,3,6,4]
+ * 输出：5
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ *
+ * 示例 2：
+ * 输入：prices = [7,6,4,3,1]
+ * 输出：0
+ * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+ *
+ * 提示：
+ * 1 <= prices.length <= 105
+ * 0 <= prices[i] <= 104
+ */
+public class MaxProfit121 {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(new MaxProfit121().maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int maxProfit = 0;
+        int maxPrice = prices[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            int cur = prices[i];
+            if (maxPrice > cur) {
+                maxProfit = Math.max(maxProfit, maxPrice - cur);
+            }
+            maxPrice = Math.max(maxPrice, cur);
+        }
+        return maxProfit;
+
+//        int n = prices.length;
+//        int[] max = new int[n];
+//        max[n - 1] = prices[n - 1];
+//        for (int i = n - 2; i > 0; i--) {
+//            max[i] = Math.max(max[i + 1], prices[i]);
+//        }
+//        int maxProfit = 0;
+//        for (int i = 0; i < n - 1; i++) {
+//            int cur = prices[i];
+//            int nextMax = max[i + 1];
+//            if (nextMax > cur) {
+//                maxProfit = Math.max(maxProfit, nextMax - cur);
+//            }
+//        }
+//        return maxProfit;
+    }
+}
+
+
+
+
+
+
+
+
+
+
