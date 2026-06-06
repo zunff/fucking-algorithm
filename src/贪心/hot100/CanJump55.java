686dc746cc1499f77b985821244ce625983f5e85
zunf <1430119384@qq.com>
2026-06-06 11:11:02 +0800

    greedy: 55 CanJump

---
+++ src/贪心/hot100/CanJump55.java
@@ -0,0 +1,76 @@
+package 贪心.hot100;
+
+/**
+ * 55. 跳跃游戏 中等
+ *
+ * 给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ * 示例 2：
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ *
+ * 提示：
+ * 1 <= nums.length <= 104
+ * 0 <= nums[i] <= 105
+ */
+public class CanJump55 {
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,0,4};
+        System.out.println(new CanJump55().canJump(nums));
+    }
+
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        // 前i个下标最远能到达哪个下标
+        int maxIndex = 0;
+        for (int i = 0; i < n; i++) {
+            // 前i个下标都没法达到当前点，那么一定到不了终点
+            if (i > maxIndex) {
+                return false;
+            }
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+        }
+        return maxIndex >= n - 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
