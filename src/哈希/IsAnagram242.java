a4f4e568a9e79605cf373a33d434fc08ca91b926
zunf <1430119384@qq.com>
2026-06-23 19:21:57 +0800

    hash: 242 IsAnagram

---
+++ src/哈希/IsAnagram242.java
@@ -0,0 +1,50 @@
+package 哈希;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 242. 有效的字母异位词 简单
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的 字母异位词。
+ *
+ * 示例 1:
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *
+ * 提示:
+ * 1 <= s.length, t.length <= 5 * 10^4
+ * s 和 t 仅包含小写字母
+ *
+ * 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ */
+public class IsAnagram242 {
+    public static void main(String[] args) {
+        System.out.println(new IsAnagram242().isAnagram("rac", "car"));
+    }
+
+
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> hashS = new HashMap<>();
+        Map<Character, Integer> hashT = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            hashS.put(c, hashS.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            hashT.put(c, hashT.getOrDefault(c, 0) + 1);
+        }
+        return hashS.equals(hashT);
+    }
+}
+
+
+
+
+
+
