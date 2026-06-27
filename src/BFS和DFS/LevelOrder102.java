b860e58c43b1d79d89a20d6a435e322572e67d8e
zunf <1430119384@qq.com>
2026-06-27 15:34:27 +0800

    bfs: 102 LevelOrder

---
+++ src/BFS和DFS/LevelOrder102.java
@@ -0,0 +1,65 @@
+package BFS和DFS;
+
+import 二叉树.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历 中等
+ *
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 示例 1：
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ *
+ * 示例 2：
+ * 输入：root = [1]
+ * 输出：[[1]]
+ *
+ * 示例 3：
+ * 输入：root = []
+ * 输出：[]
+ *
+ * 提示：
+ * 树中节点数目在范围 [0, 2000] 内
+ * -1000 <= Node.val <= 1000
+ */
+public class LevelOrder102 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        root.left = node1;
+        root.right = node2;
+        System.out.println(new LevelOrder102().levelOrder(root));
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
